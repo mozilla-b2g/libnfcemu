7349b99ea342153c26ade470ca171f5bbaa49888
Thomas Zimmermann <tdz@users.sourceforge.net>
2013-09-26 13:01:11 +0200

    Bug 916863: Added NFC console commands
    There are currently 3 console commands for NFC, which allow
    for injecting rf_discovery and rf_intf_activated notificatons,
    and data messages into the emulated NFC device.
    
    Signed-off-by: Thomas Zimmermann <tdz@users.sourceforge.net>

---
+++ src/console.c
@@ -0,0 +1,138 @@
+#include "hw/llcp.h"
+#include "hw/goldfish_nfc.h"
+#include "hw/nfc-re.h"
+#include "hw/nfc.h"
+#include "hw/nfc-nci.h"
+
+static size_t
+nfc_rf_discovery_ntf_cb(void* data,
+                        struct nfc_device* nfc,
+                        union nci_packet* ntf)
+{
+    return nfc_create_rf_discovery_ntf(data, nfc, ntf);
+}
+
+static size_t
+nfc_rf_intf_activated_ntf_cb(void* data,
+                             struct nfc_device* nfc,
+                             union nci_packet* ntf)
+{
+    return nfc_create_rf_intf_activated_ntf(data, nfc, ntf);
+}
+
+static int
+do_nfc_ntf( ControlClient  client, char*  args )
+{
+    char *p;
+
+    if (!args) {
+        return -1;
+    }
+
+    /* read notification type */
+    p = strsep(&args, " ");
+
+    if (!p) {
+        return -1;
+    }
+
+    if (!strcmp(p, "rf_discover")) {
+        /* read remote-endpoint index */
+        p = strsep(&args, " ");
+
+        if (!p) {
+            return -1;
+        }
+
+        errno = 0;
+        size_t i = strtoul(p, NULL, 0);
+
+        if (errno || !(i < sizeof(nfc_res)/sizeof(nfc_res[0])) ) {
+            return -1;
+        }
+
+        goldfish_nfc_send_ntf(nfc_rf_discovery_ntf_cb, nfc_res+i);
+    } else if (!strcmp(p, "rf_intf_activated")) {
+        struct nfc_re *re = NULL;
+
+        /* read remote-endpoint index */
+        p = strsep(&args, " ");
+
+        if (p) {
+            errno = 0;
+            size_t i = strtoul(p, NULL, 0);
+
+            if (errno || !(i < sizeof(nfc_res)/sizeof(nfc_res[0])) ) {
+                return -1;
+            }
+
+            re = nfc_res + i;
+        }
+
+        /* if re == NULL, active RE will be used */
+        goldfish_nfc_send_ntf(nfc_rf_intf_activated_ntf_cb, re);
+    }
+
+    return 0;
+}
+
+struct nfc_dta_write_param {
+  uint8_t connid;
+  size_t len;
+  const void* data;
+};
+
+static size_t
+nfc_dta_write_cb(void* data, struct nfc_device* nfc,
+                 union nci_packet* packet)
+{
+  const struct nfc_dta_write_param* param;
+
+  param = data;
+
+  return nfc_create_dta(param->data, param->len, nfc, packet);
+}
+
+static int
+do_nfc_dta( ControlClient  client, char*  args )
+{
+    char *p;
+
+    if (!args) {
+        return -1;
+    }
+
+    /* read notification type */
+    p = strsep(&args, " ");
+
+    if (!p) {
+        return -1;
+    }
+
+    if (!strcmp(p, "s") || !strcmp(p, "send") ||
+        !strcmp(p, "w") || !strcmp(p, "wr") || !strcmp(p, "write")) {
+
+        struct nfc_dta_write_param param;
+
+        /* read connection id */
+        p = strsep(&args, " ");
+
+        if (!p) {
+            return -1;
+        }
+
+        errno = 0;
+        param.connid = strtoul(p, NULL, 0);
+
+        if (errno || !param.connid || (param.connid > 254)) {
+            return -1;
+        }
+
+        param.len = strlen(args);
+        param.data = args;
+
+        goldfish_nfc_send_dta(nfc_dta_write_cb, &param);
+    }
+
+    return 0;
+}
