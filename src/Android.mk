3429ead593f33ccd23af458a9552518744e8944c
Thomas Zimmermann <tdz@users.sourceforge.net>
2014-08-12 16:47:29 +0200

    Bug 1054934: Add 64-bit library to build scripts

--- src/Android.mk
+++ src/Android.mk
@@ -16,22 +16,42 @@
 
 LOCAL_PATH := $(call my-dir)
 
+nfcemu_SRC_FILES := base64.c\
+                    cb.c \
+                    cmdline.c \
+                    llcp.c \
+                    llcp-snep.c \
+                    ndef.c \
+                    nfc.c \
+                    nfc-hci.c \
+                    nfc-nci.c \
+                    nfc-re.c \
+                    nfc-rf.c \
+                    nfc-tag.c \
+                    nfcemu.c \
+                    snep.c
+
+#
+# 32-bit library
+#
+
 include $(CLEAR_VARS)
-LOCAL_SRC_FILES := base64.c\
-                   cb.c \
-                   cmdline.c \
-                   llcp.c \
-                   llcp-snep.c \
-                   ndef.c \
-                   nfc.c \
-                   nfc-hci.c \
-                   nfc-nci.c \
-                   nfc-re.c \
-                   nfc-rf.c \
-                   nfc-tag.c \
-                   nfcemu.c \
-                   snep.c
+LOCAL_SRC_FILES := $(nfcemu_SRC_FILES)
 LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include
 LOCAL_MODULE_TAGS := optional
 LOCAL_MODULE := libnfcemu
+include $(BUILD_HOST_SHARED_LIBRARY)
+
+#
+# 64-bit library
+#
+
+include $(CLEAR_VARS)
+LOCAL_SRC_FILES := $(nfcemu_SRC_FILES)
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../include
+LOCAL_CFLAGS := -m64
+LOCAL_LDFLAGS := -m64
+LOCAL_LDLIBS := -m64
+LOCAL_MODULE_TAGS := optional
+LOCAL_MODULE := lib64nfcemu
 include $(BUILD_HOST_SHARED_LIBRARY)
